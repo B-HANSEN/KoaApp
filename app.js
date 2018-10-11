39ff3659d3f0079c9142151b37938305cbe16407
william <willshinji@gmail.com>
2018-10-11 00:24:20 -0300

    routing with koa-router

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const Koa = require('koa');
 const json = require('koa-json');
 
-const app = new Koa();
+const Router = require('koa-router');
 
+const app = new Koa();
+const router = new Router();
+
+// Json prettier middleware
 app.use(json());
 
-app.use(async ctx => ctx.body = { msg: 'Hello from Koa' });
+//default root
+// app.use(async ctx => ctx.body = { msg: 'Hello from Koa' });
+
+router.get('/koa', (ctx, next) => ctx.body = { msg: 'Hello from Koa!' });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Server Started!'))
